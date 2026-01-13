19d8592047b3392c9147c1799c17c443f6684b19
Marco <marco.savo.92@gmail.com>
2026-01-13 20:06:14 +0100

    .

--- src/firebase.ts
+++ src/firebase.ts
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyC0w-800l59tzST68KYgfNNLHtnMo0-KwM",
+  authDomain: "matrimonioagocla.firebaseapp.com",
+  projectId: "matrimonioagocla",
+  storageBucket: "matrimonioagocla.firebasestorage.app",
+  messagingSenderId: "338571759582",
+  appId: "1:338571759582:web:502a8e5731da3785e7d74f"
 };
 
 const app = initializeApp(firebaseConfig);
